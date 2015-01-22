394584095b812d8aff38ef7506744d42e602cba0
Christopher Mazey <cjmazey@gmail.com>
2015-01-22 01:05:32 -0500

    silly but working

--- 01/CreditCard/src/CreditCard.hs
+++ 01/CreditCard/src/CreditCard.hs
@@ -6,3 +6,21 @@ toDigits :: Integer -> [Integer]
 toDigits i
   | i <= 0 = []
   | otherwise = map (toInteger . digitToInt) (show i)
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev = reverse . toDigits
+
+doubleEveryOtherRev :: [Integer] -> [Integer]
+doubleEveryOtherRev [] = []
+doubleEveryOtherRev [x] = [x]
+doubleEveryOtherRev (x : x' : xs) =
+  x : 2 * x' : doubleEveryOtherRev xs
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . doubleEveryOtherRev . reverse
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . concatMap toDigits
+
+validate :: Integer -> Bool
+validate = (0 ==) . (`rem` 10) . sumDigits . doubleEveryOther . toDigits
