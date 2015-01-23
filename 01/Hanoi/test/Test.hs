c79a8108e55a08976a700659b022c9d5c7aca8f9
Christopher Mazey <cjmazey@gmail.com>
2015-01-23 15:55:15 -0500

    finish Hanoi

--- 01/Hanoi/test/Test.hs
+++ 01/Hanoi/test/Test.hs
@@ -1,5 +1,22 @@
 module Main where
 
-main :: IO ()
-main = undefined
+import Hanoi
 
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests" [testHanoi]
+
+testHanoi :: TestTree
+testHanoi = 
+  testGroup "hanoi"
+            [testCase "ex1" $
+             hanoi 2 "a" "b" "c" @=?
+             [("a","c"),("a","b"),("c","b")]]
