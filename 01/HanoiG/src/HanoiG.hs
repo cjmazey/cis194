4294885091da6dbcd1b3f30abf7d6fcc93f9caee
Christopher Mazey <cjmazey@gmail.com>
2015-01-26 23:35:53 -0500

    begin hanoi generalized

---
+++ 01/HanoiG/src/HanoiG.hs
@@ -0,0 +1,13 @@
+module HanoiG where
+
+type Peg = String
+type Move = (Peg,Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+  | n < 1 = error "hanoi: n < 1"
+  | n == 1 = [(a,b)]
+  | otherwise = 
+    hanoi (n - 1) a c b ++
+    [(a,b)] ++
+    hanoi (n - 1) c b a
---
+++ 01/HanoiG/test/Test.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import HanoiG
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests" [testHanoi]
+
+testHanoi :: TestTree
+testHanoi = 
+  testGroup "hanoi"
+            [testCase "ex1" $
+             hanoi 2 "a" "b" "c" @=?
+             [("a","c"),("a","b"),("c","b")]]
