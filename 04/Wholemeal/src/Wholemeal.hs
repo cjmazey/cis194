2ea1d4e282403c4c47a329ac196d9d999712380e
Christopher Mazey <cjmazey@gmail.com>
2015-02-03 21:18:01 -0500

    along. . .

--- 04/Wholemeal/src/Wholemeal.hs
+++ 04/Wholemeal/src/Wholemeal.hs
@@ -25,3 +25,44 @@ fun2' =
         f n
           | even n = n `div` 2
           | otherwise = 3 * n + 1
+
+-- Exercise 2
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+            deriving (Show, Eq)
+
+height :: Tree a -> Integer
+height Leaf = undefined
+height (Node _ Leaf _ Leaf) = 0
+height (Node _ l _ Leaf) = 1 + height l
+height (Node _ Leaf _ r) = 1 + height r
+height (Node _ l _ r) = 1 + max (height l) (height r)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr (flip insertTree) Leaf
+
+insertTree :: Tree a -> a -> Tree a
+
+insertTree Leaf a =
+  Node 0 Leaf a Leaf
+
+insertTree (Node _ Leaf a Leaf) a' =
+  Node 1 Leaf a (insertTree Leaf a')
+
+insertTree (Node h Leaf a r) a' =
+  Node h (insertTree Leaf a') a r
+
+insertTree (Node h l a Leaf) a' =
+  Node h l a (insertTree Leaf a')
+
+insertTree (Node _ l@(Node lh _ _ _) a r@(Node rh _ _ _)) a'
+  | lh <= rh  = Node (rh + 1) (insertTree l a') a r
+  | otherwise = Node (lh + 1) l a (insertTree r a')
+
+insertTree (Node h l a r) a' =
+  case (l, r) of
+   (Leaf, Leaf) -> Node 1 Leaf a t
+   (Leaf, _)    -> Node h t a r
+   (_, Leaf)    -> Node h l a t
+  where t = insertTree Leaf a'
