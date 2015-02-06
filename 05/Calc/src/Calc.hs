130e379ca4ee0be97e84e2646fd03035a667b436
Christopher Mazey <cjmazey@gmail.com>
2015-02-05 22:51:33 -0500

    . . .

--- 05/Calc/src/Calc.hs
+++ 05/Calc/src/Calc.hs
@@ -1,8 +1,12 @@
 module Calc where
 
 import           ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit i) = i
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
+
+evalStr :: String -> Maybe Integer
+evalStr = undefined
