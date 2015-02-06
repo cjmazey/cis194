10bcf2460b92fdfbf98a17605a1175ac3678f43b
Christopher Mazey <cjmazey@gmail.com>
2015-02-06 00:02:30 -0500

    finish ex 4

--- 05/Calc/src/Calc.hs
+++ 05/Calc/src/Calc.hs
@@ -25,3 +25,36 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit n | n <= 0 = False
+        | otherwise = True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show, Ord)
+
+instance Expr MinMax where
+  lit = MinMax
+  add = max
+  mul = min
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit = Mod7
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
