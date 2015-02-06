f20d6a195ab35660bb47e15bcd21064730bafc3c
Christopher Mazey <cjmazey@gmail.com>
2015-02-05 23:23:28 -0500

    finish ex 3

--- 05/Calc/src/Calc.hs
+++ 05/Calc/src/Calc.hs
@@ -12,3 +12,16 @@ evalStr :: String -> Maybe Integer
 evalStr s =
   do e <- parseExp Lit Add Mul s
      return $ eval e
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
