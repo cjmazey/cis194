2649d096c3c6037013d8e9a563a3a2cf3c3b3a85
Christopher Mazey <cjmazey@gmail.com>
2015-02-09 01:02:51 -0500

    monoids''

--- lec/monoids/src/Monoids.hs
+++ lec/monoids/src/Monoids.hs
@@ -1,1 +1,27 @@
 module Monoids where
+
+import           Data.Monoid
+
+newtype Cbool =
+  Cbool { unCbool :: Bool }
+  deriving (Eq, Show)
+
+newtype Dbool =
+  Dbool { unDbool :: Bool }
+  deriving (Eq, Show)
+
+instance Monoid Cbool where
+  mempty = Cbool True
+  mappend (Cbool p) (Cbool q) = Cbool $ p && q
+
+instance Monoid Dbool where
+  mempty = Dbool False
+  mappend (Dbool p) (Dbool q) = Dbool $ p || q
+
+newtype Function a b =
+  Function { unFunction :: a -> b }
+
+instance Monoid b => Monoid (Function a b) where
+  mempty = Function $ const mempty
+  mappend (Function f) (Function g) =
+    Function $ \ x -> f x <> g x
