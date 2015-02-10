c29f0eef4b1247c5215223ff5c9cab4f559efb1f
Christopher Mazey <cjmazey@gmail.com>
2015-02-10 17:50:50 -0500

    finish hw 7

--- 07/Word/src/Editor/JoinListEditor.hs
+++ 07/Word/src/Editor/JoinListEditor.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import           Buffer
+import           Buffer.JoinList
+import           Editor
+
 main :: IO ()
-main = undefined
+main = runEditor (editor :: Editor DJoinList ()) $ fromString $ unlines
+         [ "This buffer is for notes you don't want to save, and for"
+         , "evaluation of steam valve coefficients."
+         , "To load a different file, type the character L followed"
+         , "by the name of the file."
+         ]
