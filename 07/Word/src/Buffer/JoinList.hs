f4eda2a97bb8829de63e7f8b91d4bb37f8699ef6
Christopher Mazey <cjmazey@gmail.com>
2015-02-10 12:08:12 -0500

    cleanup

--- 07/Word/src/Buffer/JoinList.hs
+++ 07/Word/src/Buffer/JoinList.hs
@@ -20,9 +20,32 @@ indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ 0 (Single _ a) = Just a
 indexJ _ (Single _ _) = Nothing
-indexJ i (Append _ j k) | i < s = indexJ i j
-                        | otherwise = indexJ (i - s) k
+indexJ i (Append _ j k)
+  | i < s = indexJ i j
+  | otherwise = indexJ (i - s) k
   where s = getSize $ size $ tag j
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty = Empty
+dropJ n s@(Single _ _)
+  | n > 0 = Empty
+  | otherwise = s
+dropJ n (Append _ l1 l2)
+  | n <= s = dropJ n l1 +++ l2
+  | otherwise = dropJ (n - s) l2
+  where s = getSize $ size $ tag l1
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty = Empty
+takeJ n s@(Single _ _)
+  | n < 1 = Empty
+  | otherwise = s
+takeJ n (Append _ l1 l2)
+  | n <= s = takeJ n l1
+  | otherwise = l1 +++ takeJ (n - s) l2
+  where s = getSize $ size $ tag l1
+
+-- tests
 
 (!!?) :: [a] -> Int -> Maybe a
 []       !!? _          = Nothing
@@ -38,26 +61,8 @@ jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
 prop_indexJ :: Int -> JoinList Size Char -> Bool
 prop_indexJ i jl = indexJ i jl == jlToList jl !!? i
 
-dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-dropJ _ Empty = Empty
-dropJ n s@(Single _ _) | n > 0 = Empty
-                       | otherwise = s
-dropJ n (Append _ l1 l2)
-  | n <= s = dropJ n l1 +++ l2
-  | otherwise = dropJ (n - s) l2
-  where s = getSize $ size $ tag l1
-
 prop_dropJ :: Int -> JoinList Size Char -> Bool
 prop_dropJ n jl = jlToList (dropJ n jl) == drop n (jlToList jl)
-
-takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-takeJ _ Empty = Empty
-takeJ n s@(Single _ _) | n < 1 = Empty
-                       | otherwise = s
-takeJ n (Append _ l1 l2)
-  | n <= s = takeJ n l1
-  | otherwise = l1 +++ takeJ (n - s) l2
-  where s = getSize $ size $ tag l1
 
 prop_takeJ :: Int -> JoinList Size Char -> Bool
 prop_takeJ n jl = jlToList (takeJ n jl) == take n (jlToList jl)
