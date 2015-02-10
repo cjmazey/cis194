e258d1327b707481b3cb879621914cdcd0874490
Christopher Mazey <cjmazey@gmail.com>
2015-02-09 22:34:48 -0500

    begin ex 2

--- 07/JoinListEditor/src/JoinList.hs
+++ 07/JoinListEditor/src/JoinList.hs
@@ -1,6 +1,16 @@
 module JoinList where
 
+import           Data.Monoid
+
 data JoinList m a = Empty
                   | Single m a
                   | Append (JoinList m a) (JoinList m a)
                   deriving (Eq, Show)
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) = Append
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append j k) = tag j <> tag k
