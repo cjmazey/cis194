0360a5abd55f9325b43c9834146808d4d0a816af
Christopher Mazey <cjmazey@gmail.com>
2015-02-10 03:07:04 -0500

    test stuff

--- 07/Word/test/Test.hs
+++ 07/Word/test/Test.hs
@@ -2,8 +2,17 @@ module Main where
 
 import Buffer.JoinList
 import Buffer.JoinList.Sized
+import Control.Monad
 import Test.Tasty
 import Test.Tasty.QuickCheck
+
+-- orphan instance
+instance (Arbitrary Size) where
+  arbitrary =
+    let x :: Gen (NonNegative Int)
+        x = arbitrary
+    in do (NonNegative x') <- x
+          return $ Size x'
 
 main :: IO ()
 main = defaultMain tests
