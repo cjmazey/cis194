3389d2a8e3ed14e5ee03db5604c03c352f30e9b9
Christopher Mazey <cjmazey@gmail.com>
2015-02-10 03:29:55 -0500

    tests revealed failure!

--- 07/Word/test/Test.hs
+++ 07/Word/test/Test.hs
@@ -8,11 +8,16 @@ import Test.Tasty.QuickCheck
 
 -- orphan instance
 instance (Arbitrary Size) where
+  arbitrary = liftM (Size . getNonNegative) arbitrary
+
+-- orphan instance
+instance (Arbitrary m, Arbitrary a) => Arbitrary (JoinList m a) where
   arbitrary =
-    let x :: Gen (NonNegative Int)
-        x = arbitrary
-    in do (NonNegative x') <- x
-          return $ Size x'
+    frequency
+      [ (1, return Empty)
+      , (2, liftM2 Single arbitrary arbitrary)
+      , (1, liftM3 Append arbitrary arbitrary arbitrary)
+      ]
 
 main :: IO ()
 main = defaultMain tests
@@ -23,5 +28,5 @@ tests = testGroup "Tests" [properties]
 properties :: TestTree
 properties = testGroup "Properties"
   [ testProperty "prop_indexJ" $
-    \ x -> x + (1 :: Int) `seq` False
+    \ i l -> prop_indexJ i l
   ]
