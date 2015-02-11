6b275e32019a3fcd58a876ccc0401a622b8e33ea
Christopher Mazey <cjmazey@gmail.com>
2015-02-11 13:56:52 -0500

    finish 8.1

--- 08/Party/src/Party.hs
+++ 08/Party/src/Party.hs
@@ -1,1 +1,18 @@
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Party where
+
+import           Data.Monoid
+import           Employee
+
+glCons :: Employee -> GuestList -> GuestList
+glCons e (GL es f) =
+  GL (e : es) (empFun e + f)
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend (GL es f) (GL es' f') =
+    GL (es <> es') (f + f')
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun = max
