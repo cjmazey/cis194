904188f02b7b8eccf8925b982839f7b27680c0b4
Christopher Mazey <cjmazey@gmail.com>
2015-02-12 00:12:11 -0500

    reformat

--- 08/Party/src/Main.hs
+++ 08/Party/src/Main.hs
@@ -4,8 +4,9 @@ import           Party
 import           Paths_Party
 
 main :: IO ()
-main = let process :: String -> String
-           process = formatGuestList . maxFun . read
-       in getDataFileName "company.txt" >>=
-          readFile >>=
-          putStrLn . process
+main =
+  let process :: String -> String
+      process = formatGuestList . maxFun . read
+  in getDataFileName "company.txt" >>=
+     readFile >>=
+     putStrLn . process
--- 08/Party/src/Party.hs
+++ 08/Party/src/Party.hs
@@ -8,32 +8,40 @@ import           Employee
 
 glCons :: Employee -> GuestList -> GuestList
 glCons e (GL es f) =
-  GL (e : es) (empFun e + f)
+  GL (e : es)
+     (empFun e + f)
 
 instance Monoid GuestList where
   mempty = GL [] 0
   mappend (GL es f) (GL es' f') =
-    GL (es ++ es') (f + f')
+    GL (es ++ es')
+       (f + f')
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun = max
 
 treeFold :: (a -> [b] -> b) -> Tree a -> b
 treeFold f t =
-  f (rootLabel t) $ map (treeFold f) $ subForest t
+  f (rootLabel t) $
+  map (treeFold f) $
+  subForest t
 
 -- combineGLs :: Employee -> [GuestList] -> GuestList
 
-nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel :: Employee -> [(GuestList,GuestList)] -> (GuestList,GuestList)
 nextLevel e = f . unzip
-  where
-    f (withSubs, withoutSubs) =
-      (mappend (glCons e mempty) (mconcat withoutSubs), mconcat withSubs)
+  where f (withSubs,withoutSubs) =
+          (mappend (glCons e mempty)
+                   (mconcat withoutSubs)
+          ,mconcat withSubs)
 
 maxFun :: Tree Employee -> GuestList
 maxFun t = moreFun withBoss withoutBoss
-  where (withBoss, withoutBoss) = treeFold nextLevel t
+  where (withBoss,withoutBoss) =
+          treeFold nextLevel t
 
 formatGuestList :: GuestList -> String
-formatGuestList (GL es f) = unlines $
-  ("Total fun: " ++ show f) : sort (map empName es)
+formatGuestList (GL es f) =
+  unlines $
+  ("Total fun: " ++ show f) :
+  sort (map empName es)
