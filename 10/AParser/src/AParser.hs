de4ed04ad8fbc3263884e4e13a7260e127d2d518
Christopher Mazey <cjmazey@gmail.com>
2015-02-13 11:20:04 -0500

    hindent

--- 10/AParser/src/AParser.hs
+++ 10/AParser/src/AParser.hs
@@ -7,7 +7,8 @@ import           Data.Char
 -- represnting the input to be parsed, and succeeds or fails; if it
 -- succeeds, it returns the parsed value along with the remainder of
 -- the input.
-newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+newtype Parser a =
+  Parser {runParser :: String -> Maybe (a,String)}
 
 -- For example, 'satisfy' takes a predicate on Char, and constructs a
 -- parser which succeeds only if it sees a Char that satisfies the
@@ -15,13 +16,12 @@ newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 -- does not satisfy the predicate (or an empty input), it fails.
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
-  where
-    f [] = Nothing    -- fail on the empty input
-    f (x:xs)          -- check if x satisfies the predicate
-                        -- if so, return x along with the remainder
-                        -- of the input (that is, xs)
-        | p x       = Just (x, xs)
-        | otherwise = Nothing  -- otherwise, fail
+  where f [] = Nothing    -- fail on the empty input
+        f (x:xs)          -- check if x satisfies the predicate
+        -- if so, return x along with the remainder
+        -- of the input (that is, xs)
+          | p x = Just (x,xs)
+          | otherwise = Nothing  -- otherwise, fail
 
 -- Using satisfy, we can define the parser 'char c' which expects to
 -- see exactly the character c, and fails otherwise.
@@ -43,19 +43,20 @@ Just ('x',"yz")
 -- integers.
 posInt :: Parser Integer
 posInt = Parser f
-  where
-    f xs
-      | null ns   = Nothing
-      | otherwise = Just (read ns, rest)
-      where (ns, rest) = span isDigit xs
+  where f xs
+          | null ns = Nothing
+          | otherwise = Just (read ns,rest)
+          where (ns,rest) = span isDigit xs
 
 ------------------------------------------------------------
 -- Your code goes below here
 ------------------------------------------------------------
 
-first :: (a -> b) -> (a, c) -> (b, c)
-first f (a, c) = (f a, c)
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (a,c) = (f a,c)
 
 instance Functor Parser where
   fmap f p =
-    Parser $ fmap (first f) . runParser p
+    Parser $
+    fmap (first f) .
+    runParser p
