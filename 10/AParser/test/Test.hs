fd9f3130608f685e93ad7ef117ba9673037f95f3
Christopher Mazey <cjmazey@gmail.com>
2015-02-13 13:45:15 -0500

    add tests

---
+++ 10/AParser/test/Test.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import           AParser
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties,unitTests]
+
+properties :: TestTree
+properties =
+  testGroup "Properties"
+            [testProperty "(no properties)" $
+             (const True :: Bool -> Bool)]
+
+unitTests :: TestTree
+unitTests =
+  testGroup "Unit tests"
+            [testCase "abParser 1" $
+             runParser abParser "abcdef" @?=
+             Just (('a','b'),"cdef")
+            ,testCase "abParser 2" $
+             runParser abParser "aebcdf" @?=
+             Nothing
+            ,testCase "abParser_ 1" $
+             runParser abParser_ "abcdef" @?=
+             Just ((),"cdef")
+            ,testCase "abParser_ 2" $
+             runParser abParser_ "aebcdf" @?=
+             Nothing
+            ,testCase "intPair" $
+             runParser intPair "12 34" @?=
+             Just ([12,34],"")]
