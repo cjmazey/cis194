9741c8af817473a0dc4158755f4ef7376aed90cc
Christopher Mazey <cjmazey@gmail.com>
2015-02-15 04:43:00 -0500

    lec 11. . .

--- lec/applicative2/src/Applicative2.hs
+++ lec/applicative2/src/Applicative2.hs
@@ -13,12 +13,14 @@ u *> v = const id <$> u <*> v
 
 mapA :: Applicative f
      => (a -> f b) -> ([a] -> f [b])
-mapA = undefined
+mapA f = sequenceA . map f
 
 sequenceA :: Applicative f
           => [f a] -> f [a]
-sequenceA fs = undefined
+sequenceA =
+  foldr (liftA2 (:))
+        (pure [])
 
 replicateA :: Applicative f
            => Int -> f a -> f [a]
-replicateA = undefined
+replicateA i = liftA $ replicate i
