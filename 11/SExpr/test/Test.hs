837620cac424c85448b024da884b942fbc0870d3
Christopher Mazey <cjmazey@gmail.com>
2015-02-16 15:21:35 -0500

    tests

---
+++ 11/SExpr/test/Test.hs
@@ -0,0 +1,39 @@
+module Main where
+
+import           AParser
+import           Data.Char
+import           SExpr
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties,unitTests]
+
+properties :: TestTree
+properties =
+  testGroup "Properties"
+            [testProperty "(no properties)" $
+             (const True :: Bool -> Bool)]
+
+unitTests :: TestTree
+unitTests =
+  testGroup "Unit tests"
+            [testCase "(no unit tests)" $
+             True @?= True
+            ,testCase "zeroOrMore 1" $
+             runParser (zeroOrMore (satisfy isUpper)) "ABCdEfgH" @?=
+             Just ("ABC","dEfgH")
+            ,testCase "oneOrMore 1" $
+             runParser (oneOrMore (satisfy isUpper)) "ABCdEfgH" @?=
+             Just ("ABC","dEfgH")
+            ,testCase "zeroOrMore 2" $
+             runParser (zeroOrMore (satisfy isUpper)) "abcdeFGh" @?=
+             Just ("","abcdeFGh")
+            ,testCase "oneOrMore 2" $
+             runParser (oneOrMore (satisfy isUpper)) "abcdeFGh" @?=
+             Nothing]
