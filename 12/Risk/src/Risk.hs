d324f2c70582a578af1c2c4289d2a0a236c9845b
Christopher Mazey <cjmazey@gmail.com>
2015-02-17 01:41:21 -0500

    fin 12.2

--- 12/Risk/src/Risk.hs
+++ 12/Risk/src/Risk.hs
@@ -2,20 +2,27 @@
 
 module Risk where
 
-import Control.Monad.Random
+import           Control.Monad
+import           Control.Monad.Random
+import           Data.List
 
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
-  deriving (Eq, Ord, Show, Num)
+newtype DieValue =
+  DV {unDV :: Int}
+  deriving (Eq,Ord,Show,Num)
 
-first :: (a -> b) -> (a, c) -> (b, c)
-first f (a, c) = (f a, c)
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (a,c) = (f a,c)
 
 instance Random DieValue where
-  random           = first DV . randomR (1,6)
-  randomR (low,hi) = first DV . randomR (max 1 (unDV low), min 6 (unDV hi))
+  random =
+    first DV .
+    randomR (1,6)
+  randomR (low,hi) =
+    first DV .
+    randomR (max 1 (unDV low),min 6 (unDV hi))
 
 die :: Rand StdGen DieValue
 die = getRandom
@@ -25,4 +32,29 @@ die = getRandom
 
 type Army = Int
 
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+data Battlefield =
+  Battlefield {attackers :: Army
+              ,defenders :: Army}
+  deriving (Show)
+
+battle' :: Battlefield -> [DieValue] -> [DieValue] -> Battlefield
+battle' b aDice dDice =
+  uncurry Battlefield $
+  foldl (\(a,d) (aDie,dDie) ->
+           if aDie > dDie
+              then (a,d - 1)
+              else (a - 1,d))
+        (attackers b,defenders b)
+        (zip (sortBy (flip compare) aDice)
+             (sortBy (flip compare) dDice))
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle b =
+  do as <-
+       replicateM (min 3 (attackers b - 1))
+                  die
+     ds <-
+       replicateM (min 2 (defenders b))
+                  die
+     return $
+       battle' b as ds
